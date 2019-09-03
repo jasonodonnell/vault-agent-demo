78905d7b04774cd2491961d33839319a76600d9e
Jason O'Donnell <2160810+jasonodonnell@users.noreply.github.com>
2019-09-03 16:33:27 -0400

    Add container script

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -1,48 +1,64 @@
-#!/bin/bash
+#!/bin/ash
 
 JWT_TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
 
-function echo_err() {
+echo_err() {
     echo -e "$(date) ERROR: ${1?}"
 }
 
-function echo_info() {
+echo_info() {
     echo -e "$(date) INFO: ${1?}"
 }
 
-function echo_warn() {
+echo_warn() {
     echo -e "$(date) WARN: ${1?}"
 }
 
-function trap_sigterm() {
+trap_sigterm() {
     echo_warn "Clean shutdown of Vault Agent.."
     exit 0
 }
 
 trap 'trap_sigterm' SIGINT SIGTERM
 
-while [[ ${TOKEN} == "" ]]
+TOKEN=''
+while [[ "${TOKEN}" == "" ]]
 do
     sleep 5
     echo_info "Attempting to receive login token from Vault.."
-    TOKEN=$(vault write auth/kubernetes/login -field=token role=demo jwt=${JWT_TOKEN?})
-
+    TOKEN=$(vault write -field=token auth/kubernetes/login role=demo jwt=$JWT_TOKEN)
     if [[ ${TOKEN} == "" ]]
     then
         echo_warn "Could not receive token, trying again.."
     fi
 done
 
-echo_info "Received login token, logging in.."
+echo_info "Received login token, logging in"
 
-vault login ${TOKEN?}
+vault login -no-print token=${TOKEN?}
 if [[ $? -ne 0 ]]
 then
     echo_err "Could not login, exiting.."
     exit 1
 fi
 
+
+echo_info "Logged in. Retrieving secrets.."
+
 vault read secret/demo
+
+SECRETS=$(env | grep VAULT_SECRET)
+
+while IFS= read -r secret
+do
+   val=${secret##*=}
+   echo $val
+   vaultPath=${val%%:*}
+   localPath=${val##*:}
+   vault read -format=json ${vaultPath?} > ${localPath}
+done <<- END
+${SECRETS}
+END
 
 sleep 10000 &
 
