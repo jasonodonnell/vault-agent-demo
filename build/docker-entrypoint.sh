3dc674fb6f47b0706c1109aa93a5ee13868b2633
Jason O'Donnell <2160810+jasonodonnell@users.noreply.github.com>
2019-09-03 14:30:45 -0400

    Enhance demo script

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-set -e
+JWT_TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
+
+function echo_err() {
+    echo -e "$(date) ERROR: ${1?}"
+}
+
+function echo_info() {
+    echo -e "$(date) INFO: ${1?}"
+}
+
+function echo_warn() {
+    echo -e "$(date) WARN: ${1?}"
+}
 
 function trap_sigterm() {
     echo_warn "Clean shutdown of Vault Agent.."
@@ -8,6 +20,29 @@ function trap_sigterm() {
 }
 
 trap 'trap_sigterm' SIGINT SIGTERM
+
+while [[ ${TOKEN} == "" ]]
+do
+    sleep 5
+    echo_info "Attempting to receive login token from Vault.."
+    TOKEN=$(vault write auth/kubernetes/login -field=token role=demo jwt=${JWT_TOKEN?})
+
+    if [[ ${TOKEN} == "" ]]
+    then
+        echo_warn "Could not receive token, trying again.."
+    fi
+done
+
+echo_info "Received login token, logging in.."
+
+vault login ${TOKEN?}
+if [[ $? -ne 0 ]]
+then
+    echo_err "Could not login, exiting.."
+    exit 1
+fi
+
+vault read secret/demo
 
 sleep 10000 &
 
