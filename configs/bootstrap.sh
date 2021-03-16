d632d677eff6443110fb3e032be76787895ffa7e
Jason O'Donnell <2160810+jasonodonnell@users.noreply.github.com>
2021-03-16 13:30:31 -0400

    Add CSI

--- configs/bootstrap.sh
+++ configs/bootstrap.sh
@@ -20,6 +20,7 @@ vault policy write app /vault/userconfig/demo-vault/app-policy.hcl
 vault auth enable kubernetes
 
 vault write auth/kubernetes/config \
+   disable_iss_validation="true" \
    token_reviewer_jwt="$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" \
    kubernetes_host=https://${KUBERNETES_PORT_443_TCP_ADDR}:443 \
    kubernetes_ca_cert=@/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
--- examples/transit/cleanup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-kubectl delete deployment app -n app
-kubectl delete serviceaccount app -n app
