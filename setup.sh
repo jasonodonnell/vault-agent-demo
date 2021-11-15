1275dfc46ba6b963fa4f4295faebdf5bc83a5b25
Jason O'Donnell <2160810+jasonodonnell@users.noreply.github.com>
2021-11-15 16:58:36 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -30,12 +30,6 @@ kubectl label secret demo-vault app=vault-agent-demo \
 
 ${DIR?}/postgres/run.sh
 
-helm install csi-secrets-store https://github.com/kubernetes-sigs/secrets-store-csi-driver/blob/master/charts/secrets-store-csi-driver-0.0.19.tgz?raw=true \
-  --wait --timeout=5m \
-  --namespace=vault \
-  --set linux.image.pullPolicy="IfNotPresent" \
-  --set grpcSupportedProviders="azure;gcp;vault"
-
 helm install vault \
   --namespace="${NAMESPACE?}" \
   -f ${DIR?}/values.yaml hashicorp/vault
